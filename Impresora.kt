f38842d7b2526f72642f9a91fb4ad8882ea06a08
Daniel Cano <cd.danielcano@gmail.com>
2022-04-26 16:48:52 -0400

    Aplicación de programación funcional para el cálculo de subtotal de la factura.

--- Impresora.kt
+++ Impresora.kt
@@ -11,7 +11,17 @@ class Impresora:IImpresora {
             System.out.printf("%-15s%-10s%-10s%-10s%n",item,cantidad,precio,precioLinea)
             subtotal += precioLinea
         }
+        //Programación imperativa, usando reduce
+        val subtotal2 = cuadroFactura.getEntradas().values.reduce{
+            sum, pair -> Pair(1, sum.second*sum.first+ pair.second*pair.first)
+        }.second
+        //Programación imperativa, usando fold
+        val subtotal3 = cuadroFactura.getEntradas().values.fold(0.0){
+                sum, pair -> sum + pair.second*pair.first
+        }
         println("\n\nSubtotal:\t$subtotal")
+        println("Subtotal2:\t$subtotal2")
+        println("Subtotal3:\t$subtotal3")
         val total = subtotal - (subtotal*descuento/100)
         println("Descuento:\t$descuento%")
         println("Total:\t\t$total")
