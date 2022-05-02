13107a738b260429570a442ba487429cf24f3b61
Daniel Cano <cd.danielcano@gmail.com>
2022-05-01 20:32:31 -0400

    Changing Trabajador class

--- src/main/kotlin/Trabajador.kt
+++ src/main/kotlin/Trabajador.kt
@@ -1,31 +1,29 @@
 import kotlin.math.ceil
 import kotlin.math.floor
 
-class Trabajador (val cargo:String, private val horasBaseSemana:Int, val pagoHora:Int) {
+class Trabajador (val cargo:String, val horasBaseSemana:Int = 48, val pagoHora:Int,val unidadHorasBono:Int = 5, val unidadHorasExtra:Int = 10) {
     val horasBaseMensuales = horasBaseSemana * 4
-    val unidadHorasBono = 5
-    val unidadHorasExtra = 10
+
     var horasTrabajadas:Double = 0.0
-    var horasBonus:Int = 0
 
 
     fun calcularSalario():Int{
-        return calcularHorasAPagar()*pagoHora
+        return calcularHorasAPagar(horasTrabajadas)*pagoHora
     }
 
     fun addHours(horas:Double){
         horasTrabajadas += horas
     }
-    fun calcularHorasAPagar():Int{
+    fun calcularHorasAPagar(horasTrabajadas:Double):Int{
         //Supuestos: tiene que cumplir 10 horas exactas extra, 5 horas no cuentan para bonus.
-        if(ceil(horasTrabajadas)>=horasBaseMensuales+10){
-            var calculo = ceil(horasTrabajadas) + calcularBonus()
+        if(ceil(horasTrabajadas)>=horasBaseSemana+unidadHorasExtra){
+            var calculo = ceil(horasTrabajadas) + calcularBono()
             return (calculo).toInt()
         }else{
             return ceil(horasTrabajadas).toInt()
         }
     }
-    fun calcularBonus():Int{
+    fun calcularBono():Int{
         val horasExtraTrabajadas = (ceil(horasTrabajadas) - horasBaseMensuales)
         val numeroDeBonos = floor(horasExtraTrabajadas/unidadHorasExtra).toInt()
         val horasBono = unidadHorasBono * numeroDeBonos
