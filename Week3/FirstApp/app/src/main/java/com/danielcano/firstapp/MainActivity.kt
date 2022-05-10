373b70b170669707a3e5379f5e1b59c230c3b40f
Daniel Cano <cd.danielcano@gmail.com>
2022-05-09 23:40:31 -0400

    Activity lifeCycle, toasts and Log.d

--- Week3/FirstApp/app/src/main/java/com/danielcano/firstapp/MainActivity.kt
+++ Week3/FirstApp/app/src/main/java/com/danielcano/firstapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.danielcano.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Creating app",Toast.LENGTH_SHORT).show()
+        Log.d("LifeCycleMessages", "onCreate: App Created")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"Starting App",Toast.LENGTH_SHORT).show()
+        Log.d("LifeCycleMessages", "onStart: App Started")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"Resume stage",Toast.LENGTH_SHORT).show()
+        Log.d("LifeCycleMessages", "onResume: App Resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "App Paused", Toast.LENGTH_SHORT).show()
+        Log.d("LifeCycleMessages", "onPause: App Paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "App Stopped", Toast.LENGTH_SHORT).show()
+        Log.d("LifeCycleMessages", "onStop: App Stopped")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "App Destroyed", Toast.LENGTH_SHORT).show()
+        Log.d("LifeCycleMessages", "onDestroy: App destroyed")
     }
 }
