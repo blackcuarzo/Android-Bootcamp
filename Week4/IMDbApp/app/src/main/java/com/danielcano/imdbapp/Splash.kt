7ecf3dbb9d774c3001b3f8a54c072950fc88e497
Daniel Cano <cd.danielcano@gmail.com>
2022-05-15 21:43:37 -0400

    Basic splash screen

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
@@ -1,9 +1,9 @@
 package com.danielcano.imdbapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
