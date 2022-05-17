8f4c2048784da788b955c6015d5d0fa7b3d8ea9e
Daniel Cano <cd.danielcano@gmail.com>
2022-05-17 01:21:26 -0400

    Splash with delay

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
@@ -2,14 +2,21 @@ package com.danielcano.imdbapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val loginIntent = Intent(this,Login::class.java)
-        startActivity(loginIntent)
+
+
+        Handler().postDelayed(Runnable {
+            kotlin.run {
+                val loginIntent = Intent(this,Login::class.java)
+                startActivity(loginIntent)
+            }
+        },3000)
 
     }
 
