d4f64a28d99742f098c10121fbfdf819792db428
Daniel Cano <cd.danielcano@gmail.com>
2022-05-17 01:10:45 -0400

    Intent for Login Button. Links in registration and guest text.

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Login.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Login.kt
@@ -1,11 +1,27 @@
 package com.danielcano.imdbapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.Button
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+        val guestLink = findViewById<TextView>(R.id.guest_link)
+        guestLink.movementMethod = LinkMovementMethod.getInstance()
+
+        val registrationLink = findViewById<TextView>(R.id.registration_link)
+        registrationLink.movementMethod = LinkMovementMethod.getInstance()
+
+        val abrirNavegador = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+
+
+        val buttonLogin  = findViewById<Button>(R.id.login_button)
+        buttonLogin.setOnClickListener { startActivity(abrirNavegador)  }
     }
 }
