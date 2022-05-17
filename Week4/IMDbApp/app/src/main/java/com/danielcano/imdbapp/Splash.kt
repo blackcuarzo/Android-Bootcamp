0e69311ebc05c80637a9e362b3aebf8a0644bfb6
Daniel Cano <cd.danielcano@gmail.com>
2022-05-17 00:33:27 -0400

    Login Screen

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
@@ -1,5 +1,6 @@
 package com.danielcano.imdbapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,12 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loginIntent = Intent(this,Login::class.java)
+        startActivity(loginIntent)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 }
