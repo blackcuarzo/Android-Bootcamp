3a439a2715d2ef1c7873e4215c48e11de9bb510b
Daniel Cano <cd.danielcano@gmail.com>
2022-05-17 01:28:59 -0400

    Splash with delay using Timer because Handler is deprecated

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Splash.kt
@@ -4,19 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        Handler().postDelayed(Runnable {
-            kotlin.run {
-                val loginIntent = Intent(this,Login::class.java)
-                startActivity(loginIntent)
-            }
-        },3000)
+        val loginIntent = Intent(this,Login::class.java)
+        Timer().schedule(timerTask { startActivity(loginIntent) },3000)
 
     }
 
