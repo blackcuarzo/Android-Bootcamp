84af9f955fe3bc2372c5e4d341ee84e8c1045499
Daniel Cano <cd.danielcano@gmail.com>
2022-05-25 18:54:34 -0400

    Deleted DrawableRes annotation

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Movie.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/Movie.kt
@@ -1,14 +1,10 @@
 package com.danielcano.imdbapp
 
-import androidx.annotation.DrawableRes
-
-
 data class Movie (
     val name: String,
     val description: String,
     val year: Int,
     val actors: String,
     val rating:Float,
-    @DrawableRes
     val thumbnail:Int
     )
