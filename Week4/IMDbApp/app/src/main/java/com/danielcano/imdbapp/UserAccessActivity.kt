3d9fe4595c9ef0b4d6cb741e460101f7c83f85ff
Daniel Cano <cd.danielcano@gmail.com>
2022-05-25 23:52:18 -0400

    Changed activity name. Cleaned some warnings

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
@@ -1,34 +1,27 @@
 package com.danielcano.imdbapp
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.Button
 import android.widget.TextView
-import java.util.*
-import kotlin.concurrent.timerTask
 
 class UserAccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_user_access)
         val guestLink = findViewById<TextView>(R.id.guest_link)
         guestLink.movementMethod = LinkMovementMethod.getInstance()
 
         val registrationLink = findViewById<TextView>(R.id.registration_link)
         registrationLink.movementMethod = LinkMovementMethod.getInstance()
 
-
         val buttonLogin  = findViewById<Button>(R.id.login_button)
-
 
 //        Temporal call for SearchActivity
         val searchIntent = Intent(this,SearchActivity::class.java)
         buttonLogin.setOnClickListener { startActivity(searchIntent)  }
-
-
 
     }
 }
