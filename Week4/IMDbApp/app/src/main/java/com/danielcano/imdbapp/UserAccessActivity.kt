65f269df39dae66c8956f9360f405931f75d7b83
Daniel Cano <cd.danielcano@gmail.com>
2022-05-26 11:30:23 -0400

    Fixed fragment errors
    Used view returned by the layout inflater as an id provider. findViewById is a method available to views.
    
    Imported required libraries
    
    Used requireContext() function of fragments to provide context

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
@@ -1,11 +1,7 @@
 package com.danielcano.imdbapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
-import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.danielcano.imdbapp.access.LoginFragment
 
 class UserAccessActivity : AppCompatActivity() {
--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/access/LoginFragment.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/access/LoginFragment.kt
@@ -1,11 +1,16 @@
 package com.danielcano.imdbapp.access
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.danielcano.imdbapp.R
+import com.danielcano.imdbapp.SearchActivity
 
 class LoginFragment : Fragment() {
 
@@ -18,16 +23,16 @@ class LoginFragment : Fragment() {
 
         // Code goes here
 
-        val guestLink = findViewById<TextView>(R.id.guest_link)
+        val guestLink = view.findViewById<TextView>(R.id.guest_link)
         guestLink.movementMethod = LinkMovementMethod.getInstance()
 
-        val registrationLink = findViewById<TextView>(R.id.registration_link)
+        val registrationLink = view.findViewById<TextView>(R.id.registration_link)
         registrationLink.movementMethod = LinkMovementMethod.getInstance()
 
-        val buttonLogin  = findViewById<Button>(R.id.login_button)
+        val buttonLogin  = view.findViewById<Button>(R.id.login_button)
 
-        Temporal call for SearchActivity
-        val searchIntent = Intent(this,SearchActivity::class.java)
+        //Temporal call for SearchActivity
+        val searchIntent = Intent(requireContext(), SearchActivity::class.java)
         buttonLogin.setOnClickListener { startActivity(searchIntent)  }
 
         return view
