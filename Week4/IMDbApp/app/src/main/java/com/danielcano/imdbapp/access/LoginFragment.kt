67ccef0f69acc00bee4734911803a29718b985f7
Daniel Cano <cd.danielcano@gmail.com>
2022-05-26 09:15:19 -0400

    Prepared LoginFragment.kt to receive code.
    View is going to be modified before being returned.

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/access/LoginFragment.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/access/LoginFragment.kt
@@ -14,7 +14,11 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        // Code goes here
+
+        return view
     }
 
 }
