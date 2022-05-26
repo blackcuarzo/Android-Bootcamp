dd1dde1ac4125ea6370a8511b15ca16e9e328c42
Daniel Cano <cd.danielcano@gmail.com>
2022-05-26 09:12:20 -0400

    Commented id references because of null exception There is a null exception because the references to those resources do not exist inside activity_user_access.xml anymore, they are in fragment_login.xml.

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
@@ -12,17 +12,18 @@ class UserAccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_access)
-        val guestLink = findViewById<TextView>(R.id.guest_link)
-        guestLink.movementMethod = LinkMovementMethod.getInstance()
 
-        val registrationLink = findViewById<TextView>(R.id.registration_link)
-        registrationLink.movementMethod = LinkMovementMethod.getInstance()
-
-        val buttonLogin  = findViewById<Button>(R.id.login_button)
+//        val guestLink = findViewById<TextView>(R.id.guest_link)
+//        guestLink.movementMethod = LinkMovementMethod.getInstance()
+//
+//        val registrationLink = findViewById<TextView>(R.id.registration_link)
+//        registrationLink.movementMethod = LinkMovementMethod.getInstance()
+//
+//        val buttonLogin  = findViewById<Button>(R.id.login_button)
 
 //        Temporal call for SearchActivity
-        val searchIntent = Intent(this,SearchActivity::class.java)
-        buttonLogin.setOnClickListener { startActivity(searchIntent)  }
+//        val searchIntent = Intent(this,SearchActivity::class.java)
+//        buttonLogin.setOnClickListener { startActivity(searchIntent)  }
 
         supportFragmentManager
             .beginTransaction()
