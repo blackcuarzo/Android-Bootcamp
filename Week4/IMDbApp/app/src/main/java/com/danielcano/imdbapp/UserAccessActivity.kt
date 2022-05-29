0f3e0e77b76e0678c022d4ad1472b08bd4f4a214
Daniel Cano <cd.danielcano@gmail.com>
2022-05-29 00:04:50 -0400

    Created a toolbar with back button for UserAccessActivity.kt
    Fragment container has a bug, we have to use fragment
    
    Had to erase the background color attribute of fragment_registration.xml for the toolbar to be seen

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/UserAccessActivity.kt
@@ -2,8 +2,11 @@ package com.danielcano.imdbapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.danielcano.imdbapp.access.LoginFragmentDirections
 import com.danielcano.imdbapp.access.RegistrationFragmentDirections
 
@@ -11,6 +14,10 @@ class UserAccessActivity : AppCompatActivity(), AccessNavigator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_access)
+
+        val navControler = findNavController(R.id.nav_host_access_fragment)
+        val appBarConfiguration = AppBarConfiguration(navControler.graph)
+        findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar).setupWithNavController(navControler,appBarConfiguration)
     }
 
     override fun navigateToRegistration(view:View) {
