9e130dd34a110b9bb4beafdb0deff54e1c3518c4
Daniel Cano <cd.danielcano@gmail.com>
2022-05-30 09:17:06 -0400

    Created SearchFragment.kt and fragment_search.xml

---
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/menu/SearchFragment.kt
@@ -0,0 +1,25 @@
+package com.danielcano.imdbapp.menu
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.danielcano.imdbapp.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+
+        //Code goes here
+
+
+
+        return view
+    }
+}
