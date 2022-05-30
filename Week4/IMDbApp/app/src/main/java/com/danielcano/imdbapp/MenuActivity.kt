86c3cbfe5198c4f75fb2be2a0a24986f9a9d98f1
Daniel Cano <cd.danielcano@gmail.com>
2022-05-30 19:49:18 -0400

    Prepared MenuActivity.kt for navigation
    id of navhostfragments must differ

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/MenuActivity.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/MenuActivity.kt
@@ -3,6 +3,10 @@ package com.danielcano.imdbapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.danielcano.imdbapp.menu.SearchFragment
@@ -11,6 +15,11 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val navController = findNavController(R.id.nav_host_fragment2)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val bottomNavigationMenu = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationMenu.setupWithNavController(navController)
+//        NavigationUI.setupWithNavController(bottomNavigationMenu, navController, true)
 
     }
 }
