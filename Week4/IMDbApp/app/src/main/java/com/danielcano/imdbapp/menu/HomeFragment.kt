249ac26fe1a8929d114ba1d5af0c6d4b26c018a5
Daniel Cano <cd.danielcano@gmail.com>
2022-06-01 23:10:54 -0400

    Worked on CollapsingToolbar and Coordinator Layout

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/menu/HomeFragment.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/menu/HomeFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.danielcano.imdbapp.R
 
 class HomeFragment: Fragment() {
@@ -15,7 +20,9 @@ class HomeFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-
+        val navController = findNavController()
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setupWithNavController(navController)
 
 
         return view
