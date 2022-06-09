956a7577d90d257ccf3e50ebadcb83e48b329f91
Daniel Cano <cd.danielcano@gmail.com>
2022-06-09 19:18:56 -0400

    Cleaned dependencies

--- Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/MenuActivity.kt
+++ Week4/IMDbApp/app/src/main/java/com/danielcano/imdbapp/MenuActivity.kt
@@ -1,22 +1,17 @@
 package com.danielcano.imdbapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.commit
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.danielcano.imdbapp.menu.SearchFragment
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val navController = findNavController(R.id.nav_host_fragment2)
-        val bottomNavigationMenu = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNavigationMenu =
+            findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationMenu.setupWithNavController(navController)
 
     }
