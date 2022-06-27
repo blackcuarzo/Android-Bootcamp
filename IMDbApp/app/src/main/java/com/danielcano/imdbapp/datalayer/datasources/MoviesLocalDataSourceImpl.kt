c080aeb2178ddaf1220b4e3c8704dbe32ad70952
Daniel Cano <cd.danielcano@gmail.com>
2022-06-26 20:02:41 -0400

    Erased the local data I created since I'm getting this data from the network now. Erased MoviesLocalDataSourceImpl.kt too.

--- IMDbApp/app/src/main/java/com/danielcano/imdbapp/datalayer/datasources/MoviesLocalDataSourceImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.danielcano.imdbapp.datalayer.datasources
-
-import com.danielcano.imdbapp.datalayer.requestMoviesSource
-
-class MoviesLocalDataSourceImpl() : MoviesLocalDataSource {
-    override suspend fun getMoviesList(): List<MovieDtoLocal> {
-        return requestMoviesSource()
-    }
-}
