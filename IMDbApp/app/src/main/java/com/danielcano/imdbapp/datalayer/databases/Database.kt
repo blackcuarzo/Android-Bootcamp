407be32684d4612eced85d93d2b4165346b723b6
Daniel Cano <cd.danielcano@gmail.com>
2022-06-26 23:40:30 -0400

    Created room Database

---
+++ IMDbApp/app/src/main/java/com/danielcano/imdbapp/datalayer/databases/Database.kt
@@ -0,0 +1,9 @@
+package com.danielcano.imdbapp.datalayer.databases
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1)
+abstract class UserDatabase: RoomDatabase() {
+    abstract fun userDao():UserDao
+}
