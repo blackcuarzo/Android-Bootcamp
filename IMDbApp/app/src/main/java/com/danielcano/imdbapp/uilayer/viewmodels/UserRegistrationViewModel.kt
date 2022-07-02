4ca8a276fdebe44f1ff031716ad047ddde878aa4
Daniel Cano <cd.danielcano@gmail.com>
2022-07-02 19:14:10 -0400

    Error: This error is not visible from the testing interface, where I initialize a UserValidationUseCaseImpl object. But when added userValidationUseCase in my viewModel, even if I'm not doing anything with it, the program crashes.
    A reason testing environment is not detecting it, might be that testing is asyncronous.

--- IMDbApp/app/src/main/java/com/danielcano/imdbapp/uilayer/viewmodels/UserRegistrationViewModel.kt
+++ IMDbApp/app/src/main/java/com/danielcano/imdbapp/uilayer/viewmodels/UserRegistrationViewModel.kt
@@ -1,14 +1,21 @@
 package com.danielcano.imdbapp.uilayer.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.danielcano.imdbapp.datalayer.repositories.UserRepositoryImpl
+import com.danielcano.imdbapp.domainlayer.usecases.UserValidationUseCaseImpl
 
-class UserRegistrationViewModel: ViewModel() {
-    fun registerOrValidateUser(name: String, mail: String, pass: String):Boolean {
-        return when{
+class UserRegistrationViewModel : ViewModel() {
+
+    var userValidationUseCase: UserValidationUseCaseImpl = UserValidationUseCaseImpl(UserRepositoryImpl())
+
+    fun registerOrValidateUser(name: String, email: String, pass: String): Boolean {
+        return when {
             name.isNullOrEmpty() -> false
-            mail.isNullOrEmpty() -> false
+            email.isNullOrEmpty() -> false
             pass.isNullOrEmpty() -> false
-            else -> true//Create User and return true
+            else -> {
+                true//Create User and return true
+            }
         }
     }
 }
