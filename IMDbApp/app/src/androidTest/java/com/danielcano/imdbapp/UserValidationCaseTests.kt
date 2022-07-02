9eaba9709d14106185ab487b87673664e648dc7c
Daniel Cano <cd.danielcano@gmail.com>
2022-07-02 13:40:25 -0400

    Created UserValidationUseCase and UserRepository Interfaces with a couple of fake implementations to test them

---
+++ IMDbApp/app/src/androidTest/java/com/danielcano/imdbapp/UserValidationCaseTests.kt
@@ -0,0 +1,57 @@
+package com.danielcano.imdbapp
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.danielcano.imdbapp.datalayer.databases.User
+import com.danielcano.imdbapp.datalayer.databases.UserDatabase
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class UserValidationCaseTests {
+//    private lateinit var userDatabase: UserDatabase
+
+//    @Before
+//    fun setUpDatabase() {
+//        var context = ApplicationProvider.getApplicationContext<Context>()
+//        userDatabase = Room.inMemoryDatabaseBuilder(context, UserDatabase::class.java).build()
+//    }
+
+    @Test
+    fun userRepository_getUser_userMail_success(){
+        val userRepositoryFake = UserRepositoryFakeImpl()
+        val user = User(email = "carlos.gmail.com", name = "carlos", password = "1234")
+        userRepositoryFake.addUserToDataBase(user)
+
+        assert(userRepositoryFake.getUser(user.email)!!.email == user.email)
+    }
+
+    @Test
+    fun userValidationUseCase_registerUser_success(){
+        val userValidationUseCaseFake = UserValidationUseCaseFakeImpl(UserRepositoryFakeImpl())
+        val user1 = User(name = "Carlos", email = "carlos@gmail.com", password = "12345")
+        val user2 = User(name = "Juan", email = "juan@gmail.com", password = "123")
+        userValidationUseCaseFake.registerUser(user1)
+        userValidationUseCaseFake.registerUser(user2)
+        assert(user2 == userValidationUseCaseFake.userRepository.getUser(user2.email))
+    }
+
+    @Test
+    fun userValidationUseCase_validateUser_userMailAndUserPass_success(){
+        val userValidationUseCaseFake = UserValidationUseCaseFakeImpl(UserRepositoryFakeImpl())
+        val user1 = User(name = "Carlos", email = "carlos@gmail.com", password = "12345")
+        val user2 = User(name = "Juan", email = "juan@gmail.com", password = "123")
+        userValidationUseCaseFake.registerUser(user1)
+        userValidationUseCaseFake.registerUser(user2)
+        assert(userValidationUseCaseFake.validateUser(userMail = "carlos@gmail.com", userPass = "12345"))
+    }
+
+    @Test
+    fun userValidationUseCase_validateUser_fail(){
+        val userValidationUseCaseFake = UserValidationUseCaseFakeImpl(UserRepositoryFakeImpl())
+        val user1 = User(name = "Carlos", email = "carlos@gmail.com", password = "12345")
+        val user2 = User(name = "Juan", email = "juan@gmail.com", password = "123")
+        userValidationUseCaseFake.registerUser(user1)
+        userValidationUseCaseFake.registerUser(user2)
+        assert(!userValidationUseCaseFake.validateUser("juan@gmail.com","12345"))
+    }
+}
---
+++ IMDbApp/app/src/androidTest/java/com/danielcano/imdbapp/UserValidationUseCaseFake.kt
@@ -0,0 +1,53 @@
+package com.danielcano.imdbapp
+
+import com.danielcano.imdbapp.datalayer.databases.User
+
+class UserValidationUseCaseFakeImpl(val userRepository: UserRepository) :
+    UserValidationUseCase {
+
+    override fun registerUser(user: User) {
+        userRepository.addUserToDataBase(user)
+    }
+    override fun validateUser(userMail: String, userPass: String): Boolean {
+        val user = userRepository.getUser(userMail)
+        user?.let{
+            if(it.password == userPass){
+                return true
+            }
+        }
+        return false
+    }
+}
+
+interface UserValidationUseCase {
+    fun registerUser(user: User)
+    fun validateUser(userMail: String, userPass: String): Boolean
+}
+
+
+interface UserRepository {
+    fun addUserToDataBase(user: User)
+    fun getUser(userMail: String): User?
+    fun getUsers(): List<User>
+}
+
+class UserRepositoryFakeImpl : UserRepository {
+    private var userList: MutableList<User> = mutableListOf()
+
+    override fun addUserToDataBase(user: User) {
+        userList.add(user)
+    }
+
+    override fun getUser(userMail: String): User? {
+        userList.forEach { user ->
+            if (userMail == user.email) {
+                return user
+            }
+        }
+        return null
+    }
+
+    override fun getUsers(): List<User> {
+        return userList
+    }
+}
