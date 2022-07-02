8bf778a2f970209c9488f28065b5c8ac04359226
Daniel Cano <cd.danielcano@gmail.com>
2022-07-02 13:42:44 -0400

    Created a couple of implementations as a copy of the fake ones to start developing over them

--- IMDbApp/app/src/androidTest/java/com/danielcano/imdbapp/UserValidationUseCaseFake.kt
+++ IMDbApp/app/src/androidTest/java/com/danielcano/imdbapp/UserValidationUseCaseFake.kt
@@ -8,10 +8,29 @@ class UserValidationUseCaseFakeImpl(val userRepository: UserRepository) :
     override fun registerUser(user: User) {
         userRepository.addUserToDataBase(user)
     }
+
     override fun validateUser(userMail: String, userPass: String): Boolean {
         val user = userRepository.getUser(userMail)
-        user?.let{
-            if(it.password == userPass){
+        user?.let {
+            if (it.password == userPass) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+class UserValidationUseCaseImpl(val userRepository: UserRepository) :
+    UserValidationUseCase {
+
+    override fun registerUser(user: User) {
+        userRepository.addUserToDataBase(user)
+    }
+
+    override fun validateUser(userMail: String, userPass: String): Boolean {
+        val user = userRepository.getUser(userMail)
+        user?.let {
+            if (it.password == userPass) {
                 return true
             }
         }
@@ -32,6 +51,27 @@ interface UserRepository {
 }
 
 class UserRepositoryFakeImpl : UserRepository {
+    private var userList: MutableList<User> = mutableListOf()
+
+    override fun addUserToDataBase(user: User) {
+        userList.add(user)
+    }
+
+    override fun getUser(userMail: String): User? {
+        userList.forEach { user ->
+            if (userMail == user.email) {
+                return user
+            }
+        }
+        return null
+    }
+
+    override fun getUsers(): List<User> {
+        return userList
+    }
+}
+
+class UserRepositoryImpl : UserRepository {
     private var userList: MutableList<User> = mutableListOf()
 
     override fun addUserToDataBase(user: User) {
