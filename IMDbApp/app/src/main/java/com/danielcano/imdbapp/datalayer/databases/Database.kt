6b6fcca3e2527984091cf1c0861aab62e988249d
Daniel Cano <cd.danielcano@gmail.com>
2022-07-02 21:49:00 -0400

    Error continues, trying to handle it

--- IMDbApp/app/src/main/java/com/danielcano/imdbapp/datalayer/databases/Database.kt
+++ IMDbApp/app/src/main/java/com/danielcano/imdbapp/datalayer/databases/Database.kt
@@ -7,11 +7,31 @@ import androidx.room.RoomDatabase
 import androidx.test.core.app.ApplicationProvider
 
 @Database(entities = [User::class], version = 1)
-abstract class UserDatabase: RoomDatabase() {
-    abstract fun userDao():UserDao
+abstract class UserDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+        private var instance: UserDatabase? = null
+
+        fun getInstance(context: Context): UserDatabase {
+            if (instance == null) {
+                instance = buildDatabase(context)
+            }
+            return instance!!
+        }
+
+        private fun buildDatabase(context: Context): UserDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                UserDatabase::class.java,
+                "userDatabase"
+            ).build()
+
+        }
+    }
 }
 
-object Database{
+object Database {
     val instance by lazy {
         val context = ApplicationProvider.getApplicationContext<Context>()
         Room.inMemoryDatabaseBuilder(context, UserDatabase::class.java).build()
--- IMDbApp/app/src/main/java/com/danielcano/imdbapp/datalayer/repositories/UserRepositoryImpl.kt
+++ IMDbApp/app/src/main/java/com/danielcano/imdbapp/datalayer/repositories/UserRepositoryImpl.kt
@@ -1,15 +1,18 @@
 package com.danielcano.imdbapp.datalayer.repositories
 
+import android.service.autofill.UserData
+import androidx.test.core.app.ApplicationProvider
 import com.danielcano.imdbapp.datalayer.databases.Database
 import com.danielcano.imdbapp.datalayer.databases.User
+import com.danielcano.imdbapp.datalayer.databases.UserDatabase
 
 class UserRepositoryImpl() : UserRepository {
 
     override fun addUserToDataBase(user: User) {
-        Database.instance.userDao().addUser(user)
+        UserDatabase.getInstance(ApplicationProvider.getApplicationContext()).userDao().addUser(user)
     }
 
     override fun getUser(userMail: String): User? {
-        return Database.instance.userDao().getUserByEmail(userMail)
+        return UserDatabase.getInstance(ApplicationProvider.getApplicationContext()).userDao().getUserByEmail(userMail)
     }
 }
